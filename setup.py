01fc04efe45bf595db11783fad04eb61a01b1283
Kim Hyunjun <kim@hyunjun.kr>
2012-02-13 15:41:17 +0900

    Create `setup.py`.
    Created `setup.py`.
    Also created `LICENSE`. This project will be distributed under MIT License.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+
+readme = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+
+setup(
+    name='Padak',
+    version='0.1',
+    description='EDSL template engine written in Python',
+    url='https://github.com/kimjayd/padak',
+    long_description=readme,
+    author='Hyunjun Kim',
+    author_email='kim@hyunjun.kr',
+    license='MIT License',
+    packages=['padak']
+)
